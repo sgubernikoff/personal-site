e42683e42a5af4f4786cdfb71eb0620f609569ec
Sam Gubernikoff <sgubernikoff@gmail.com>
2022-05-31 12:01:27 -0400

    typo

--- src/Navbar.js
+++ src/Navbar.js
@@ -58,7 +58,7 @@ function NavBar() {
             <a
               target="_blank"
               className="handle"
-              href="https://indd.adobe.com/view/2a7d2a66-d2ad-43a3-a3c4-699d6802ff19"
+              href="https://indd.adobe.com/view/71a62bf4-d4ad-42b9-aee8-f2896f65a17a"
             >
               <img src={resume} className="nav-icon" />
             </a>
